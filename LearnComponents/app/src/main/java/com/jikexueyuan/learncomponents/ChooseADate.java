68716aee1b1fb1d411fa12f499964cbbf1ce3ebf
changchou <chang_chou@126.com>
2016-11-17 18:33:53 +0800

    2-1-Android 常用控件

---
+++ LearnComponents/app/src/main/java/com/jikexueyuan/learncomponents/ChooseADate.java
@@ -0,0 +1,34 @@
+package com.jikexueyuan.learncomponents;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+public class ChooseADate extends AppCompatActivity {
+
+    private Button btnChooseDte;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_adate);
+
+        btnChooseDte = (Button) findViewById(R.id.btnChooseDate);
+        btnChooseDte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ChooseADate.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        String theDate = String.format("%d-%d-%d", year, monthOfYear + 1, dayOfMonth);
+                        System.out.println(theDate);//手机App选择4月，系统会输出3，所以要+1
+                        btnChooseDte.setText(theDate);
+                    }
+                },2015,3,15).show();//月份显示是从0开始，传3手机App显示4月
+            }
+        });
+    }
+}
